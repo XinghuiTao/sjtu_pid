ab095ff1effc811816ff456593c08e7efc222d17
boringtao <taoxh.uk@gmail.com>
2021-11-06 15:51:03 +0000

    Add: deap GA process

--- src/drone_pid/scripts/yaw_simple_pid_deap.py
+++ src/drone_pid/scripts/yaw_simple_pid_deap.py
@@ -123,3 +123,15 @@ if __name__ == '__main__':
 # The proportional term (Kp*proportional_error): helps us to reduce the rise time. 
 # The integral term(Ki*integral_error): helps us to reduce any steady-state error.
 # The derivative term(Kd*derivative_error): helps us to prevents any overshoot.
+
+# GA
+# [Start] Generate random population of n chromosomes (suitable solutions for the problem)
+# [Fitness] Evaluate the fitness f(x) of each chromosome x in the population
+#  [New population] Create a new population by repeating following steps until the new population is complete
+#    [Selection] Select two parent chromosomes from a population according to their fitness (the better fitness, the bigger chance to be selected)
+#    [Crossover] With a crossover probability cross over the parents to form a new offspring (children). If no crossover was performed, offspring is an exact copy
+#    [Mutation] With a mutation probability mutate new offspring at each locus (position in chromosome).
+#    [Accepting] Place new offspring in a new population
+# [Replace] Use new generated population for a further run of algorithm
+# [Test] If the end condition is satisfied, stop, and return the best solution in current population
+# [Loop] Go to step 2
